bbe0f62152fbdf35558c0962ce3701462b833713
Rathish77 <rathishit2016@gmail.com>
2022-03-13 14:49:48 +0530

    Fibonacci added

---
+++ src/org/fib/Fibonacci.java
@@ -0,0 +1,25 @@
+package org.fib;
+
+public class Fibonacci {
+	private void sample() {
+
+		int pf = 1, sf = 0, cr = 0;
+				
+		for(int i = 1; i<=15; i++) {
+			
+			System.out.println(cr);
+			
+			sf = pf;
+			pf = cr + pf;
+			cr = sf;
+			
+			
+		}
+	}
+		public static void main(String[] args) {
+			Fibonacci h = new Fibonacci();
+		h.sample();
+		}
+		
+
+}
